618011a580285729f078d99e2dd17df1b31127d3
bernardo <bernardo_nf@hotmail.com>
2024-11-28 16:52:59 -0300

    feat: add authentication middleware

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,16 +1,20 @@
 const { Router } = require("express")
 
 const NotesController = require('../controllers/NotesController')
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const notesRoutes = Router()
 
 const notesController = new NotesController()
 
+// Middleware de autenticação para todas as rotas
+notesRoutes.use(ensureAuthenticated)
+
 // Criando rotas para as funcionalidades
-notesRoutes.get("/", notesController.index)
-notesRoutes.post("/:user_id", notesController.create)
+notesRoutes.post("/", notesController.create)
 notesRoutes.get("/:id", notesController.show)
 notesRoutes.delete("/:id", notesController.delete)
+notesRoutes.get("/", notesController.index)
 
 
 module.exports = notesRoutes
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require("express")
 
 const UsersController = require('../controllers/UsersController')
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+
 
 const usersRoutes = Router()
 
@@ -8,5 +10,5 @@ const usersController = new UsersController()
 
 // Criando rotas para as funcionalidades
 usersRoutes.post("/", usersController.create)
-usersRoutes.put("/:id", usersController.update)
+usersRoutes.put("/", ensureAuthenticated, usersController.update)
 module.exports = usersRoutes
